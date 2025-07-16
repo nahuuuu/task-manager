7070be2d2dbeb73072f8efe83239b2c1b32696fd
Nahuel <nahuellmallea@gmail.com>
2025-07-15 21:21:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# task-manager-
+gestor de tareas
---
+++ src/main/java/com/taskmanager/TaskmanagerV1Application.java
@@ -0,0 +1,13 @@
+package com.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerV1Application.class, args);
+	}
+
+}
